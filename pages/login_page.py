3b367ae97e4c060d6b378cc364196867606d55b6
sh-devlog <81398747+sh-devlog@users.noreply.github.com>
2026-03-01 02:40:32 +0900

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,14 +14,3 @@ class LoginPage:
 
     def click_login(self):
         self.driver.find_element(By.CSS_SELECTOR, "input[value='로그인']").click()
-
-
-# alert 객체 가져오기
-#alert = driver.switch_to.alert
-
-# 결과 메시지 출력
-# alert 텍스트 가져오기 (로그인 실패 시)
-#message = alert.text
-#print("로그인 결과:", message)
-# 확인 버튼 클릭
-#alert.accept()
